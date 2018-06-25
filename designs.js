d20f1696cf75ea965f50d91f29b51280313b1dca
Didja08 <36296572+Didja08@users.noreply.github.com>
2018-06-25 18:33:00 +0200

    designs.js

--- designs.js
+++ designs.js
@@ -1,61 +1,25 @@
 
-(function(document){
-    'use strict';
-
-    const elements = {
-        colorPicker: document.getElementById('colorPicker'),
-        gridCanvas: document.getElementById('pixelCanvas'),
-        widthInput: document.getElementById('input_width'),
-        heightInput: document.getElementById('input_height')
-    };
-
-    const init = function() {
-
-document.getElementById('sizePicker').addEventListener('submit', makeGrid, false);
-
-elements.gridCanvas.addEventListener('click', setGridColor);
-    };
-
-    function makeGrid(event) {
-        event.preventDefault();
-
-         const gridSize = getGridSize();
-
-
-        clearCanvas();
-
-
-        for (let row = 0; row < gridSize.numberOfRows; row++) {
-            let tr = elements.gridCanvas.insertRow(row);
-
-
-            for (let col = 0; col < gridSize.numberOfColumns; col++) {
-                tr.insertCell(col);
-            }
-        }
-    }
-
-    function setGridColor(event) {
-        let color = elements.colorPicker.value;
-
-        event.target.setAttribute('style', 'background-color: ' + color);
-    }
-
-    function clearCanvas() {
-        elements.gridCanvas.innerHTML = '';
-    }
-
-
-    function getGridSize() {
-        let numberOfRows = elements.heightInput.value;
-        let numberOfColumns = elements.widthInput.value;
-
-        return {
-            numberOfColumns: parseInt(numberOfColumns),
-            numberOfRows: parseInt(numberOfRows)
-        }
-    }
-
-    init();
-
-}(document));
+$(document).ready(function(){  
+  $('#sizePicker').submit(function (event){ 
+    const gridHeight = $('#inputHeight').val();
+    const gridWidth = $('#inputWeight').val();
+    event.preventDefault(); 
+    makeGrid(gridHeight, gridWidth); 
+  }); 
+  function makeGrid(height,width){
+    $('tr').remove();
+    var i=1;
+    while (i<=height) { 
+      i++; 
+      $('#pixelCanvas').append('<tr id=table></tr>');
+      for (let j=1; j<=width; j++){ 
+        $('tr:last').append('<td></td>'); 
+        $('td').attr("class",'cells');  
+      } 
+    } 
+    $('.cells').click(function (event){ 
+      const cellColor = $('#colorPicker').val(); 
+      $(event.target).css('background-color', cellColor); 
+    }) 
+  } 
+});
